c9ed4a8ac9cdaa293f46f7f68581dccd9dadfdd3
Meher-ak <akermi.meher@gmail.com>
2024-06-03 18:00:19 +0100

    Add real-time tag logic

--- frontend/libs/home/src/lib/home.store.ts
+++ frontend/libs/home/src/lib/home.store.ts
@@ -1,8 +1,10 @@
 import { Injectable } from '@angular/core';
 import { ComponentStore, OnStateInit, tapResponse } from '@ngrx/component-store';
 import { pipe } from 'rxjs';
-import { switchMap } from 'rxjs/operators';
+import { concatMap, switchMap } from 'rxjs/operators';
 import { HomeService } from './home.service';
+import { WSState } from '@infordevjournal/ws/src';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
 
 export interface HomeState {
   tags: string[];
@@ -10,12 +12,13 @@ export interface HomeState {
 
 @Injectable()
 export class HomeStoreService extends ComponentStore<HomeState> implements OnStateInit {
-  constructor(private readonly homeService: HomeService) {
+  constructor(private readonly homeService: HomeService, private readonly wsState: WSState) {
     super({ tags: [] });
   }
 
   ngrxOnStateInit() {
     this.getTags();
+    this.listenToSocketTag();
   }
 
   // SELECTORS
@@ -29,6 +32,25 @@ export class HomeStoreService extends ComponentStore<HomeState> implements OnSta
           tapResponse(
             (response) => {
               this.patchState({ tags: response.tags });
+            },
+            (error) => {
+              console.error('error getting tags: ', error);
+            },
+          ),
+        ),
+      ),
+    ),
+  );
+
+  readonly listenToSocketTag = this.effect<void>(
+    pipe(
+      concatMap(() =>
+        this.wsState.tag.pipe(
+          takeUntilDestroyed(),
+          tapResponse(
+            (tag) => {
+              console.log('taaaag',tag)
+              this.patchState({ tags: [tag, ...this.get().tags] });
             },
             (error) => {
               console.error('error getting tags: ', error);
